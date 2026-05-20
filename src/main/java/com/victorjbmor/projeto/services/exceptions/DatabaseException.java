91b7f6fd51897440851d3aa52ac1f58e9a6829bb
Joao Moraes <victorjbmoraes@gmail.com>
2026-05-20 08:06:18 -0300

    Exception handling - delete

---
+++ src/main/java/com/victorjbmor/projeto/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.victorjbmor.projeto.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
